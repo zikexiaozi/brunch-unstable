fd25aaf27256930aaab6416cd71b45ee6d190b71
Su Yue <l@damenly.org>
2024-01-26 12:36:04 +0800

    Do not sign kernel
    Signed-off-by: Su Yue <l@damenly.org>

--- build_kernels.sh
+++ build_kernels.sh
@@ -12,11 +12,7 @@ for kernel in $kernels; do
 	echo "Building kernel $kernel"
 	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make -C "./kernels/$kernel" -j"$NTHREADS" O=out || { echo "Kernel build failed"; exit 1; }
 	rm -f "./kernels/$kernel/out/source"
-	if [ -f /persist/keys/brunch.priv ] && [ -f /persist/keys/brunch.pem ]; then
-		echo "Signing kernel $kernel"
-		mv "./kernels/$kernel/out/arch/x86/boot/bzImage" "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" || { echo "Kernel signing failed"; exit 1; }
-		sbsign --key /persist/keys/brunch.priv --cert /persist/keys/brunch.pem "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" --output "./kernels/$kernel/out/arch/x86/boot/bzImage" || { echo "Kernel signing failed"; exit 1; }
-	fi
+
 	echo "Including kernel $kernel headers"
 	srctree="./kernels/$kernel"
 	objtree="./kernels/$kernel/out"
